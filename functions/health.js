895e77e78e4fcbba72688484c6a5a818427fd2c7
maceryanarmarillereyes-lgtm <maceryanarmarillereyes@gmail.com>
2026-02-06 08:53:41 +0800

    fix3

---
+++ functions/health.js
@@ -0,0 +1,8 @@
+export async function onRequest() {
+  return new Response("ok", {
+    headers: {
+      "content-type": "text/plain; charset=utf-8",
+      "cache-control": "no-store"
+    }
+  });
+}
