836043e066bb547b5f56b396bfcb0debf6705d32
maceryanarmarillereyes-lgtm <maceryanarmarillereyes@gmail.com>
2026-02-25 20:22:36 +0800

    21313

--- public/js/pages/controller_lab.js
+++
@@ -1,9 +0,0 @@
-/* File: public/js/pages/controller_lab.js */
-(window.Pages = window.Pages || {}, window.Pages.controller_lab = function(root){
-  if(!root) return;
-  root.innerHTML = `
-    <div class="card pad" style="min-height:420px;display:flex;align-items:center;justify-content:center;">
-      <div class="small muted">Controller Lab page is ready for implementation.</div>
-    </div>
-  `;
-});
