d677e864b0f94d851b83dd68f7ac13202d210a71
maceryanarmarillereyes-lgtm <maceryanarmarillereyes@gmail.com>
2026-03-04 14:56:50 +0800

    Harden normalizeSettings export interop for route runtime

--- server/lib/normalize_settings.js
+++ server/lib/normalize_settings.js
@@ -1,6 +1,11 @@
 /* @AI_CRITICAL_GUARD: UNTOUCHABLE ZONE. */
 // Resolve normalizeSettings from the correct root-level lib path
 const path = require('path');
-const { normalizeSettings } = require(path.resolve(__dirname, '../../lib/normalizeSettings'));
+
+const normalizedModule = require(path.resolve(__dirname, '../../lib/normalizeSettings'));
+const normalizeSettings =
+  (normalizedModule && normalizedModule.normalizeSettings)
+  || (normalizedModule && normalizedModule.default)
+  || normalizedModule;
 
 module.exports = { normalizeSettings };
